d42ce63e26e0038f5ce1baf2bf5cac332a6f1d28
ccadevida03 <c.cadevida03@gmail.com>
2025-08-29 18:15:31 +0800

    updated db

--- services/databaseService.php
+++ services/databaseService.php
@@ -6,18 +6,18 @@ date_default_timezone_set("Asia/Manila");
 class Database
 {
     // local
-    private $host = "localhost";
-    private $db_name = "avalon";
-    private $username = "root";
-    private $password = '';
+    // private $host = "localhost";
+    // private $db_name = "avalon";
+    // private $username = "root";
+    // private $password = '';
 
 
     //prod
 
-    // private $host = "216.218.206.42";
-    // private $db_name = "qualitre_sysdb";
-    // private $username = "qualitre_admin";
-    // private $password = 'wD?j{)4yY+UML&b#';
+    private $host = "216.218.206.42";
+    private $db_name = "avalonwo_system";
+    private $username = "avalonwo_admin";
+    private $password = ']=?tcMar*xZ56Z^?';
 
 
 
