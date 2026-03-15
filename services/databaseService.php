4005d50225c0e0222cf477e59a0e2f144dd92499
ccadevida03 <c.cadevida03@gmail.com>
2026-03-15 11:53:04 +0800

    updated allowed cors origin

--- services/databaseService.php
+++ services/databaseService.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 // Only allow requests from this origin
 $isLocal = false;
-$allowedOrigin = "https://staging.avalonwoundcare.ph";
+$allowedOrigin = "https://system.avalonwoundcare.ph";
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 // Check if request comes from allowed origin
